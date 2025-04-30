4d74288c5fbe57a06122d8314e823d2bac40e8dc
FujiApaul <ped.iglesia@gmail.com>
2025-04-30 09:52:36 +0800

    fixed viewport width and height

---
+++ core/src/main/java/io/github/grace/ni/fernan/FernansGrace.java
@@ -0,0 +1,25 @@
+package io.github.grace.ni.fernan;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class FernansGrace extends Game {
+
+    public SpriteBatch batch;
+    public FitViewport viewport;
+
+    @Override
+    public void create() {
+
+        viewport = new FitViewport(16, 9);
+        batch = new SpriteBatch();
+
+        this.setScreen(new MainFernan(this));
+    }
+
+    public void render(){
+        super.render();
+    }
+}
--- core/src/main/java/io/github/grace/ni/fernan/MainFernan.java
+++ core/src/main/java/io/github/grace/ni/fernan/MainFernan.java
@@ -1,11 +1,77 @@
 package io.github.grace.ni.fernan;
 
-import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.ScreenUtils;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class MainFernan extends Game {
+/** First screen of the application. Displayed after the application is created. */
+public class MainFernan implements Screen {
+
+    final FernansGrace game;
+
+    Texture MainMenuBG;
+
+    public MainFernan(final FernansGrace game){
+        this.game = game;
+
+        MainMenuBG = new Texture("BG1.png");
+    }
+
+
     @Override
-    public void create() {
-        setScreen(new FirstScreen());
+    public void show() {
+        // Prepare your screen here.
+    }
+
+    @Override
+    public void render(float delta) {
+        // Draw your screen here. "delta" is the time since last render in seconds.
+
+        ScreenUtils.clear(Color.BLACK);
+
+        game.viewport.apply();
+        game.batch.setProjectionMatrix(game.viewport.getCamera().combined);
+
+        game.batch.begin();
+
+        float worldWidth = game.viewport.getWorldWidth();
+        float worldHeight = game.viewport.getWorldHeight();
+
+        game.batch.draw(MainMenuBG, 0, 0 , worldWidth, worldHeight);
+
+        game.batch.end();
+
+//        if (Gdx.input.isTouched()){
+//            dispose();
+//        }
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // Resize your screen here. The parameters represent the new window size.
+        game.viewport.update(width, height, true);
+    }
+
+    @Override
+    public void pause() {
+        // Invoked when your application is paused.
+    }
+
+    @Override
+    public void resume() {
+        // Invoked when your application is resumed after pause.
+    }
+
+    @Override
+    public void hide() {
+        // This method is called when another screen replaces this one.
+    }
+
+    @Override
+    public void dispose() {
+        // Destroy screen's assets here.
     }
 }
